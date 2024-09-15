20253ee32674d891cbfd4028992f8f7d3c2afcc2
yeison <jeison1500@hotmail.com>
2024-09-15 18:36:01 -0500

    papa

---
+++ script.js
@@ -0,0 +1,406 @@
+const departamentosCiudades = 
+{
+    "AMAZONAS": [
+      "LETICIA",
+      "PUERTO NARIÑO"
+    ],
+    "ANTIOQUIA": [
+      "MEDELLÍN",
+      "APARTADÓ",
+      "BELLO",
+      "ENVIGADO",
+      "ITAGÜÍ",
+      "RIONEGRO",
+      "TURBO",
+      "LA CEJA",
+      "LA UNIÓN",
+      "CAUCASIA",
+      "SABANETA",
+      "CARMEN DE VIBORAL",
+      "URABÁ"
+    ],
+    "ARAUCA": [
+      "ARAUCA",
+      "ARAUQUITA",
+      "CRAVO NORTE",
+      "FORTUL",
+      "PUERTO RONDON",
+      "SARAVENA"
+    ],
+    "ATLÁNTICO": [
+      "BARRANQUILLA",
+      "SOLEDAD",
+      "MALAMBO",
+      "SABANALARGA",
+      "SABANAGRANDE",
+      "CAMPO DE LA CRUZ",
+      "JUAN DE ACOSTA",
+      "LURUACO",
+      "PALMAR DE VARELA"
+    ],
+    "BOLÍVAR": [
+      "CARTAGENA",
+      "MAGANGUÉ",
+      "TURBANÁ",
+      "ARJONA",
+      "CLEMENCIA",
+      "MOMPOX",
+      "SANTA CATALINA",
+      "SAN JUAN NEPOMUCENO",
+      "MARÍA LA BAJA",
+      "VILLANUEVA"
+    ],
+    "BOYACÁ": [
+      "TUNJA",
+      "DUITAMA",
+      "SOGAMOSO",
+      "PAIPA",
+      "CHIQUINQUIRÁ",
+      "CÓMBITA",
+      "NOBSA",
+      "TIBASOSA",
+      "VILLA DE LEYVA"
+    ],
+    "CALDAS": [
+      "MANIZALES",
+      "VILLAMARÍA",
+      "CHINCHINÁ",
+      "RIOSUCIO",
+      "LA DORADA",
+      "SUPÍA",
+      "NEIRA",
+      "PÁCORA"
+    ],
+    "CAQUETÁ": [
+      "FLORENCIA",
+      "EL PAUJÍL",
+      "LA MONTAÑITA",
+      "MORELIA",
+      "PUERTO RICO",
+      "SOLANO",
+      "SAN VICENTE DEL CAGUÁN",
+      "CURILLO"
+    ],
+    "CASANARE": [
+      "YOPAL",
+      "AGUAZUL",
+      "HATO COROZAL",
+      "MONTERREY",
+      "RECETOR",
+      "TÁMARA",
+      "VILLANUEVA",
+      "PAZ DE ARIPORO",
+      "SABANALARGA"
+    ],
+    "CAUCA": [
+      "POPAYÁN",
+      "PASTO",
+      "CAUCA",
+      "EL TAMBO",
+      "GUAPI",
+      "JAMBALÓ",
+      "LA VEGA",
+      "LOPEZ DE MICAY",
+      "PIENDAMÓ"
+    ],
+    "CESAR": [
+      "VALLEDUPAR",
+      "AGUACHICA",
+      "BOSCONIA",
+      "LA PAZ",
+      "GAMARRA",
+      "PUEBLO BELLO",
+      "SAN DIEGO",
+      "SAN MARTÍN"
+    ],
+    "CHOCÓ": [
+      "QUIBDÓ",
+      "ACANDÍ",
+      "BAHÍA SOLANO",
+      "CARMEN DEL DARIÉN",
+      "ISTMINA",
+      "JURADÓ",
+      "RÍO IRÓ",
+      "RÍO QUITO"
+    ],
+    "CÓRDOBA": [
+      "MONTERÍA",
+      "PLANETA RICA",
+      "SAHAGÚN",
+      "TIERRALTA",
+      "LORICA",
+      "CERETÉ",
+      "CIÉNAGA DE ORO",
+      "SAN ANTERO",
+      "SAN BERNARDO DEL VIENTO"
+    ],
+    "CUNDINAMARCA": [
+      "BOGOTÁ",
+      "CHÍA",
+      "SOACHA",
+      "ZIPAQUIRÁ",
+      "CAJICÁ",
+      "MOSQUERA",
+      "FUNZA",
+      "FUSAGASUGÁ",
+      "COTA",
+      "TENA"
+    ],
+    "GUAINÍA": [
+      "INÍRIDA"
+    ],
+    "GUAVIARE": [
+      "SAN JOSÉ DEL GUAVIARE",
+      "CALAMAR",
+      "EL RETORNO",
+      "EL TAMBO",
+      "MIRAFLORES",
+      "PUERTO COLOMBIA",
+      "PUERTO GUZMÁN"
+    ],
+    "HUILA": [
+      "NEIVA",
+      "PITALITO",
+      "GARZÓN",
+      "LA PLATA",
+      "CAMPOALEGRE",
+      "TIMANÁ",
+      "ALTAMIRA",
+      "ISNOS",
+      "OPORAPA"
+    ],
+    "LA GUAJIRA": [
+      "RIOHACHA",
+      "MAICAO",
+      "URIBIA",
+      "SAN JUAN DEL CESAR",
+      "FONSECA",
+      "VILLANUEVA",
+      "LA JAGUA DEL PILAR",
+      "HATONUEVO"
+    ],
+    "MAGDALENA": [
+      "SANTA MARTA",
+      "EL BANCO",
+      "CIÉNAGA",
+      "FUNCIÓN",
+      "ARACATACA",
+      "EL RETÉN",
+      "GUACHACA",
+      "MINCA",
+      "SABANAS DE SAN ÁNGEL",
+      "PIVIJAY"
+    ],
+    "META": [
+      "VILLAVICENCIO",
+      "ACACÍAS",
+      "PUERTO LÓPEZ",
+      "PUERTO RICO",
+      "SAN MARTÍN",
+      "CUMARAL",
+      "MESETAS",
+      "VISTAHERMOSA"
+    ],
+    "NARIÑO": [
+      "PASTO",
+      "IPIALES",
+      "TUMACO",
+      "PUPIALES",
+      "SANDONÁ",
+      "YACUANQUER",
+      "CHACHAGÜÍ",
+      "EL CHARCO"
+    ],
+    "NORTE DE SANTANDER": [
+      "CÚCUTA",
+      "OCAÑA",
+      "ÁBREGO",
+      "CHINÁCOTA",
+      "LOS PATIOS",
+      "VILLA DEL ROSARIO",
+      "SAN CAYETANO",
+      "LA PLAYA",
+      "EL ZULIA"
+    ],
+    "PUTUMAYO": [
+      "MOCOA",
+      "PUERTO ASÍS",
+      "SAN FRANCISCO",
+      "VILLAGARZÓN",
+      "PUERTO CAICEDO",
+      "LEGUÍZAMO",
+      "SANTIAGO"
+    ],
+    "QUINDÍO": [
+      "ARMENIA",
+      "CALARCÁ",
+      "MONTENEGRO",
+      "SALENTO",
+      "FILANDIA"
+    ],
+    "RISARALDA": [
+      "PEREIRA",
+      "DOSQUEBRADAS",
+      "LA VIRGINIA",
+      "APIA",
+      "BALBOA",
+      "SANTA ROSA DE CABAL",
+      "SANTUARIO"
+    ],
+    "SAN ANDRÉS Y PROVIDENCIA": [
+      "SAN ANDRÉS",
+      "PROVIDENCIA"
+    ],
+    "SANTANDER": [
+      "BUCARAMANGA",
+      "BARRANCABERMEJA",
+      "GIRÓN",
+      "FLORIDA BLANCA",
+      "PIEDECUESTA",
+      "SAN GIL",
+      "SOCORRO",
+      "VÉLEZ"
+    ],
+    "SUCRE": [
+      "SINCELEJO",
+      "COROZAL",
+      "SAMPUÉS",
+      "TOLÚ",
+      "COVENAS",
+      "SAN ONOFRE",
+      "SAN BENITO ABAD",
+      "OVEJAS"
+    ],
+    "TOLIMA": [
+      "IBAGUÉ",
+      "ESPINAL",
+      "MELGAR",
+      "HONDA",
+      "PIEDRAS",
+      "EL ESPINAL",
+      "LÉRIDA",
+      "CHAPARRAL"
+    ],
+    "VALLE DEL CAUCA": [
+      "CALI",
+      "BUENAVENTURA",
+      "PALMIRA",
+      "TULUÁ",
+      "BUGA",
+      "YUMBO",
+      "JAMUNDÍ",
+      "GUACARÍ"
+    ],
+    "VAUPÉS": [
+      "MITÚ"
+    ],
+    "VICHADA": [
+      "PUERTO CARREÑO",
+      "CUMARIBO",
+      "LA PRIMAVERA",
+      "SANTA ROSALÍA"
+    ]
+  }
+  
+
+
+
+document.addEventListener('DOMContentLoaded', function () {
+    const departamentosSelect = document.getElementById('department');
+    const ciudadesSelect = document.getElementById('city');
+
+    // Cargar departamentos en el select
+    Object.keys(departamentosCiudades).forEach(departamento => {
+        const option = document.createElement('option');
+        option.value = departamento;
+        option.textContent = departamento.charAt(0).toUpperCase() + departamento.slice(1);
+        departamentosSelect.appendChild(option);
+    });
+
+    // Evento para actualizar ciudades cuando se selecciona un departamento
+    departamentosSelect.addEventListener('change', function () {
+        const ciudades = departamentosCiudades[this.value];
+
+        // Limpiar el select de ciudades
+        ciudadesSelect.innerHTML = '<option value="" disabled selected>Selecciona una ciudad</option>';
+
+        // Llenar el select de ciudades para el departamento seleccionado
+        ciudades.forEach(ciudad => {
+            const option = document.createElement('option');
+            option.value = ciudad;
+            option.textContent = ciudad;
+            ciudadesSelect.appendChild(option);
+        });
+    });
+});
+
+
+document.getElementById('customerForm').addEventListener('submit', function (e) {
+    e.preventDefault();
+
+    const name = document.getElementById('name').value;
+    const surname = document.getElementById('surname').value;
+    const address = document.getElementById('address').value;
+    const department = document.getElementById('department').value;
+    const city = document.getElementById('city').value;
+    const phone = document.getElementById('phone').value;
+
+    if (phone.length !== 10 || !/^\d{10}$/.test(phone)) {
+        Swal.fire({
+            icon: 'error',
+            title: 'Error',
+            text: 'El número de teléfono debe tener exactamente 10 dígitos.',
+        });
+        return;  // No enviar el formulario si no es válido
+    }
+
+    // Deshabilitar el botón de enviar
+    const submitButton = document.querySelector('button[type="submit"]');
+    submitButton.disabled = true;
+
+    // URL del script de Google Apps Script
+    const scriptURL = 'https://script.google.com/macros/s/AKfycbz5AC4Edi60VYSqodM2aInhcizgF6-EBYc2JGrVa5nAG89kk-4jV3Q44PTHiKZFgmKS/exec';  // Reemplaza con tu URL de Web App
+
+    // Crear el objeto de datos para enviar al script
+    const formData = new FormData();
+    formData.append('name', name);
+    formData.append('surname', surname);
+    formData.append('address', address);
+    formData.append('department', department);
+    formData.append('city', city);
+    formData.append('phone', phone);
+
+    // Enviar los datos a la hoja de cálculo usando fetch
+    fetch(scriptURL, { method: 'POST', body: formData })
+        .then(response => response.text())
+        .then(responseText => {
+            Swal.fire({
+                icon: 'success',
+                title: 'Éxito',
+                text: 'Formulario enviado correctamente',
+            });
+
+            // Construir el mensaje para WhatsApp
+            const whatsappNumber = '573242378757';  // Reemplaza con el número de WhatsApp en formato internacional, sin el "+"
+            const whatsappMessage = `Datos de cliente:\n\nNombre: ${name}\nApellidos: ${surname}\nDirección: ${address}\nDepartamento: ${department}\nCiudad: ${city}\nTeléfono: ${phone}`;
+
+            // Codificar el mensaje para que sea compatible con una URL
+            const whatsappURL = `https://wa.me/${whatsappNumber}?text=${encodeURIComponent(whatsappMessage)}`;
+
+            // Redirigir al usuario a WhatsApp con el mensaje prellenado
+            window.open(whatsappURL, '_blank');
+
+            // Borrar los datos del formulario
+            document.getElementById('customerForm').reset();
+        })
+        .catch(error => {
+            console.error('Error al enviar los datos:', error);
+            submitButton.disabled = false;  // Volver a habilitar el botón si hubo un error
+
+            Swal.fire({
+                icon: 'error',
+                title: 'Error',
+                text: 'Hubo un problema al enviar el formulario. Inténtalo de nuevo.',
+            });
+        });
+});
